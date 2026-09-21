4d2f5e45e67a5e67970d8b78b763e903ccbad8ee
Uczen <Uczen@LAPTOP-26CG11IL>
2026-09-21 14:14:57 +0200

    koniec lekcji

--- Programowanie/EventVsBindingMauiApp/MainPage.xaml.cs
+++ Programowanie/EventVsBindingMauiApp/MainPage.xaml.cs
@@ -2,8 +2,6 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
@@ -11,17 +9,21 @@
 
         private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            if(rotationSlider is not null)
-            {
-                rotationLabel.Rotation = rotationSlider.Value;
-            }
+            /*if (rotationSlider is not null)
+                rotationLabel.Rotation = rotationSlider.Value;*/
+
+            /*
+            Slider slider = sender as Slider;
+            if (slider is not null
+                && rotationLabel is not null)
+                rotationLabel.Rotation = slider.Value;
+
+            */
+
+            if (sender is Slider slider
+                && rotationLabel is not null)
+                rotationLabel.Rotation = slider.Value;
         }
-        private void Slider_ValueChanged2(object sender, ValueChangedEventArgs e)
-        {
-            if (rotationSlider2 is not null)
-            {
-                rotationLabel.Rotation = rotationSlider2.Value;
-            }
-        }
+
     }
 }
